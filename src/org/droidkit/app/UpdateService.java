7e1eb4755afdc4116b4a7696e7df8c2357deee97
Mike Novak <mike.novak@medialets.com>
2010-09-25 18:55:32 -0400

    boilerplate for the UpdateService

---
+++ src/org/droidkit/app/UpdateService.java
@@ -0,0 +1,29 @@
+/*
+ * Copyright 2010 Mike Novak <michael.novakjr@gmail.com>
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.droidkit.app;
+
+/**
+ * Provides an easy over the air update service for applications that are not distributed via
+ * any marketplace. If your application is used in a market of some kind this is not a recommended
+ * service.
+ * 
+ * @version 1
+ * @since 1
+ * @author mrn
+ */
+public class UpdateService {
+
+}
